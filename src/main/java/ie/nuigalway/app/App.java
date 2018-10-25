f2497845d9adf929e7ed47c46daa0cbbb299957c
eddied96 <eddied96@gmail.com>
2018-10-25 18:00:13 +0100

    Second Commit, maven files

---
+++ src/main/java/ie/nuigalway/app/App.java
@@ -0,0 +1,13 @@
+package ie.nuigalway.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
